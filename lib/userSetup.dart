c67fd1f409b458c6a1001015b90b3c16e10f73e6
Rana Sharjeel Ali <89970169+RanaSharjeelShji@users.noreply.github.com>
2023-12-15 20:20:00 +0500

    Update userSetup.dart

--- lib/userSetup.dart
+++ lib/userSetup.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-class MyModel extends ChangeNotifier {
+class MyModel extends ChangeNotifier{
   int _counter = 0;
   int get counter => _counter;
-
-  increment() {
-    _counter += 1;
+  increaseValue() {
+    _counter++;
     notifyListeners();
   }
 }
+// Step onE  make a class with any name;
+//aik variable bna lo
+// uska getter setter bnalo
+// functionality k liye function bna lo
+// functionlaity lo notifyListner lga do
+//au main screen pr jao
