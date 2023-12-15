f6197012956abd8e20bf5dcc579326890eed5649
Rana Sharjeel Ali <89970169+RanaSharjeelShji@users.noreply.github.com>
2023-12-15 20:18:54 +0500

    Update screenone.dart

--- lib/screenone.dart
+++ lib/screenone.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutterprovidor/screentwo.dart';
-import 'package:flutterprovidor/userSetup.dart';
 import 'package:provider/provider.dart';
+import 'package:providertutorial/countProvider.dart';
+import 'package:providertutorial/screenTwo.dart';
 
 class ScreenOne extends StatefulWidget {
   const ScreenOne({super.key});
@@ -15,35 +15,36 @@ class _ScreenOneState extends State<ScreenOne> {
   Widget build(BuildContext context) {
     MyModel myModel = Provider.of<MyModel>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "I am screen two",
-        ),
-      ),
-      body: Center(
-          child: Column(
-        children: [
-          Text(
-            myModel.counter.toString(),
-            style: const TextStyle(fontSize: 50),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const ScreenTwo(),
-                    ));
-              },
-              child: const Text("Screen Two"))
-        ],
-      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          myModel.increment();
+          myModel.increaseValue();
         },
         child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: const Text("screen one"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              myModel.counter.toString(),
+              style: const TextStyle(fontSize: 40),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ScreenTwo(),
+                      ));
+                },
+                child: const Text("Next Screen"))
+          ],
+        ),
       ),
     );
   }
 }
+// Class ka name ko variale type rkho gaay. aur provider k through usko get kro gaay bilkul scaffold k oper
+// user ka jo variable ka name rkha tha osko use name.(any thing from your class function or variable)
